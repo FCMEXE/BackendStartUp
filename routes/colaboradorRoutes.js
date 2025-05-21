e49d7d79bc8afec26305654e38af7b10218e51b4
FCMEXE <Semcamargo@gmail.com>
2025-05-21 15:54:01 -0300

    ATT README

--- routes/colaboradorRoutes.js
+++ routes/colaboradorRoutes.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const colaboradorController = require('../controllers/colaboradorController');
+
+// Middlewares de autenticação
 const { protegerRotaColaborador } = require('../middleware/authColaboradorMiddleware');
+const { protegerRotaGerente } = require('../middleware/authMiddleware'); // <- Importa o middleware do gerente
 
 // Rotas públicas
 router.post('/registrar', colaboradorController.registrarColaborador);
 router.post('/login', colaboradorController.loginColaborador);
 
-// Rotas protegidas (apenas colaboradores autenticados)
+// Rota protegida por GERENTE (listar todos os colaboradores)
+router.get('/', protegerRotaGerente, colaboradorController.getTodosColaboradores);
+
+// Rotas protegidas por COLABORADOR
 router.get('/perfil', protegerRotaColaborador, colaboradorController.getPerfilColaborador);
-router.get('/', protegerRotaColaborador, colaboradorController.getTodosColaboradores);
 router.get('/:id', protegerRotaColaborador, colaboradorController.getColaboradorPorId);
 
 module.exports = router;
